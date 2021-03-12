a8c324cd0a7204a59f9a4cb2718863da4b15e706
John Benavides <john.elric@gmail.com>
2021-03-12 18:46:18 +0100

    revert backend port to 5000

--- workspaces/backend/src/server_config.js
+++ workspaces/backend/src/server_config.js
@@ -1,5 +1,5 @@
 export const ServerConfig = {
-  server_port: 4000,
+  server_port: 5000,
   arangodb_url: process.env.DB_URL || "http://localhost:8529",
   arangodb_encoded_ca: "",
   arangodb_database: process.env.DB_DATABASE || "arangobnb",
