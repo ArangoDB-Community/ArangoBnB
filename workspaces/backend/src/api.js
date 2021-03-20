a9a96eacc32342fc2fe0fecc8cab8a922cd8a6d4
John Benavides <john.elric@gmail.com>
2021-03-20 12:27:24 +0100

    add send response to the context

--- workspaces/backend/src/api.js
+++ workspaces/backend/src/api.js
@@ -1,31 +1,24 @@
 import KoaRouter from '@koa/router';
 import { aql } from 'arangojs';
 
-function sendResponse(ctx, response, status = 200) {
-  ctx.status = status;
-  ctx.body = JSON.stringify(response);
-  ctx.type = 'application/json';
-}
-
 const ApiRouter = new KoaRouter();
 
 ApiRouter.use('(.*)', async (ctx, next) => {
+  ctx.sendResponse = (response, status = 200) => {
+    ctx.status = status;
+    ctx.body = JSON.stringify(response);
+    ctx.type = 'application/json';
+  };
   try {
     await next();
   } catch (e) {
     console.error(e);
-    sendResponse(
-      ctx,
-      {
-        error: e.message,
-      },
-      500,
-    );
+    ctx.sendResponse({ error: e.message }, 500);
   }
 });
 
 ApiRouter.all('/api/test', async (ctx) => {
-  sendResponse(ctx, {
+  ctx.sendResponse({
     text: 'Hello, world!',
   });
 });
@@ -37,44 +30,43 @@ ApiRouter.get('/api/results', async (ctx) => {
     RETURN results
     `);
 
-  sendResponse(ctx, await cursor.all());
+  ctx.sendResponse(await cursor.all());
 });
 
 ApiRouter.post('/api/mapResults', async (ctx) => {
-  try {
-    const { body } = ctx.request;
-    const [north, east, south, west] = body.mapArea;
-    const poly = [
-      [west, north],
-      [east, north],
-      [east, south],
-      [west, south],
-      [west, north],
-    ];
-    const additionalCriteria = [];
+  const { body } = ctx.request;
+  const [north, east, south, west] = body.mapArea;
+  const poly = [
+    [west, north],
+    [east, north],
+    [east, south],
+    [west, south],
+    [west, north],
+  ];
+  const additionalCriteria = [];
 
-    if (body.roomType && body.roomType.length) {
-      additionalCriteria.push(aql`
+  if (body.roomType && body.roomType.length) {
+    additionalCriteria.push(aql`
         AND
         ANALYZER(${body.roomType} ANY IN listing.room_type, "identity")
       `);
-    }
+  }
 
-    if (body.amenities && body.amenities.length) {
-      additionalCriteria.push(aql`
+  if (body.amenities && body.amenities.length) {
+    additionalCriteria.push(aql`
         AND
         ANALYZER(${body.amenities} ALL IN listing.amenities, "identity")
     `);
-    }
+  }
 
-    if (body.priceRange && body.priceRange.length) {
-      additionalCriteria.push(aql`
+  if (body.priceRange && body.priceRange.length) {
+    additionalCriteria.push(aql`
         AND
         IN_RANGE(listing.price, ${body.priceRange[0]}, ${body.priceRange[1]}, true, true)
       `);
-    }
+  }
 
-    const cursor = await ctx.db.query(aql`
+  const cursor = await ctx.db.query(aql`
     LET listings = (
       FOR listing IN arangobnb
       SEARCH ANALYZER(GEO_CONTAINS(GEO_POLYGON(${poly}), listing.location), "geo")
@@ -92,12 +84,9 @@ ApiRouter.post('/api/mapResults', async (ctx) => {
         RETURN item
         )
         RETURN {listings, amenities}
-        `);
+    `);
 
-    sendResponse(ctx, await cursor.all());
-  } catch (e) {
-    sendResponse(e.message);
-  }
+  ctx.sendResponse(await cursor.all());
 });
 
 ApiRouter.get('/api/filters', async (ctx) => {
@@ -110,11 +99,11 @@ ApiRouter.get('/api/filters', async (ctx) => {
         RETURN item
     `);
 
-  sendResponse(ctx, await cursor.all());
+  ctx.sendResponse(await cursor.all());
 });
 
 ApiRouter.all('(.*)', async (ctx) => {
-  sendResponse(ctx, { text: 'Nothing to see here' }, 404);
+  ctx.sendResponse({ text: 'Nothing to see here' }, 404);
 });
 
 export { ApiRouter };
